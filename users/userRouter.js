09c4498c97159e5b72b9c4dc6cf547be8b3eae3b
Zack Spring Chief <jzacksc@gmail.com>
2023-05-27 00:20:31 -0600

    fixing but

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
 //TOFUCKINGDO
+export const newUsers = express.Router();
+
 users.Router("/users", (req, res) => {
   console.log("hello user");
+});
+
+Router.get("/users", (req, res) => {
+  console.log("is this correct?");
 });
